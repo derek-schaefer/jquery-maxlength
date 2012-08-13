761fdaf35ff3380856ac9db0af57f1d760a46bfa
Derek Schaefer <derek.schaefer@gmail.com>
2012-08-13 10:35:32 -0500

    Added ability to specify status element

--- jquery.maxlength.js
+++ jquery.maxlength.js
@@ -4,6 +4,7 @@
 
         var options = $.extend({
             max: 10,
+            status: null,
             statusClass: 'status',
             statusText: 'characters left'
         }, settings);
@@ -12,13 +13,21 @@
 
             var $el = $(this);
             var len = $el.val().length;
+            var $status = options.status;
+
+            if ($status === null) {
+                $status = $('<div class="' + options.statusClass + '"></div>');
+                $el.after($status);
+            } else {
+                $status.addClass(options.statusClass);
+            }
 
             function update() {
                 var left = options.max - len;
                 if (left < 0) {
                     left = 0;
                 }
-                $el.siblings('div.' + options.statusClass).html(left + ' ' + options.statusText);
+                $status.html(left + ' ' + options.statusText);
             }
 
             function check() {
@@ -28,7 +37,6 @@
                 update();
             }
 
-            $el.after('<div class="' + options.statusClass + '"></div>');
             update();
 
             $el.keyup(function() {
